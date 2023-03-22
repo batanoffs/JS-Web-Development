f86e6624d16b4121f863f385d5cca242870b71e1
batanoff <batanoff.s@protonmail.com>
2023-03-22 17:02:26 +0200

    fix cleverLily

--- 01. Basics/08. For Loop - Exercise/cleverLily.js
+++ 01. Basics/08. For Loop - Exercise/cleverLily.js
@@ -1,6 +1,35 @@
 function cleverLily(input) {
-  let lillyAge = input[0];
-  let washingMachinePrice = input[1];
-  let pricePerToy = input[2];
+  let lillyAge = Number(input[0]);
+  let washingMachinePrice = Number(input[1]);
+  let pricePerToy = Number(input[2]);
+  let toyCount = 0;
+  let brotherMoneyTaken = 1;
+  let birthdayMoneyCounter = 0;
+  let birthdayMoneyTotal = 0;
+
+  for (let i = 1; i <= lillyAge; i++) {
+    if (i === 2) {
+      birthdayMoneyCounter = 10;
+      birthdayMoneyTotal += birthdayMoneyCounter;
+    } else if (i % 2 === 0) {
+      birthdayMoneyCounter += 10;
+      birthdayMoneyTotal += birthdayMoneyCounter;
+      brotherMoneyTaken++;
+    } else {
+      toyCount++;
+    }
+  }
+
+  let moneyFromToys = toyCount * pricePerToy;
+  let totalMoney = moneyFromToys + birthdayMoneyTotal - brotherMoneyTaken;
+  let moneyLeft = totalMoney - washingMachinePrice;
+  let insufficientMoney = washingMachinePrice - totalMoney;
+
+  if (totalMoney >= washingMachinePrice) {
+    console.log(`Yes! ${moneyLeft.toFixed(2)}`);
+  } else {
+    console.log(`No! ${insufficientMoney.toFixed(2)}`);
+  }
 }
-cleverLily(["10", "170.00", "6"]);
+
+cleverLily(["21", "1570.98", "3"]);
