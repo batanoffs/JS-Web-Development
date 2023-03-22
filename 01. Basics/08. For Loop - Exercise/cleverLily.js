b6f2cb989fdbd8497791632fe8f37ca35d113ecf
batanoff <batanoff.s@protonmail.com>
2023-03-22 15:12:04 +0200

    test

--- 01. Basics/08. For Loop - Exercise/cleverLily.js
+++ 01. Basics/08. For Loop - Exercise/cleverLily.js
@@ -1,7 +1,6 @@
-function cleverLily (input) {
+function cleverLily(input) {
   let lillyAge = input[0];
   let washingMachinePrice = input[1];
   let pricePerToy = input[2];
-
 }
-cleverLily(["10","170.00","6"]);
+cleverLily(["10", "170.00", "6"]);
