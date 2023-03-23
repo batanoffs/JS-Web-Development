7d6dbc7b2e8277b573825b0ac6d4197d29af0fe5
batanoff <batanoff.s@protonmail.com>
2023-03-23 22:46:38 +0200

    fix specialNumbers

--- 02. Fundamentals/04. Lab Data Types and Variables/specialNumbers.js
+++ 02. Fundamentals/04. Lab Data Types and Variables/specialNumbers.js
@@ -2,8 +2,11 @@ function specialNumbers(n) {
   for (let i = 1; i <= n; i++) {
     let newNumber = i;
     let sumOfDigits = 0;
-
-    switch (i) {
+    while (newNumber > 0) {
+      sumOfDigits += newNumber % 10;
+      newNumber = Math.floor(newNumber / 10);
+    }
+    switch (sumOfDigits) {
       case 5: case 7: case 11:
         console.log(`${i} -> True`);
         break;
@@ -11,30 +14,6 @@ function specialNumbers(n) {
         console.log(`${i} -> False`);
         break;
     }
-
-    while (newNumber > 0) {
-      sumOfDigits += newNumber % 10;
-      newNumber = Math.floor(i / 10);
-    }    
   }
 }
-specialNumbers(15);
-
-
-// function printSpecialNumbers(n) {
-//   for (let i = 1; i <= n; i++) {
-//     let sumOfDigits = 0;
-//     let number = i;
-//     while (number > 0) {
-//       sumOfDigits += number % 10;
-//       number = Math.floor(number / 10);
-//     }
-//     let isSpecial = (sumOfDigits === 5 || sumOfDigits === 7 || sumOfDigits === 11);
-//     console.log(i + " -> " + isSpecial);
-//   }
-// }
-
-// // Example usage
-// printSpecialNumbers(15);
-
-
+specialNumbers(20);
