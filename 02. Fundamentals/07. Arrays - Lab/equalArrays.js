97a1551ece5e38773a310fe93cb01f7a2ce83866
batanoff <batanoff.s@protonmail.com>
2023-03-30 23:55:31 +0300

    practice Arrays lab

--- 02. Fundamentals/07. Arrays - Lab/equalArrays.js
+++ 02. Fundamentals/07. Arrays - Lab/equalArrays.js
@@ -1,4 +1,16 @@
-function equalArrays() {
+function equalArrays(firstArr, secondArr) {
+  let difference = 0;
+  let sum = 0;
+  for (let i = 0; i < firstArr.length; i++) {
+    const firstArrElement = Number(firstArr[i]);
+    const secondArrElement = Number(secondArr[i]);
+    sum += firstArrElement;
 
+    if (firstArrElement !== secondArrElement) {
+     difference = i;
+     return console.log(`Arrays are not identical. Found difference at ${difference} index`);
+    }    
+  }  
+  console.log(`Arrays are identical. Sum: ${sum}`);  
 }
-equalArrays(['10','20','30'], ['10','20','30']);
+equalArrays(['1'], ['10']);
