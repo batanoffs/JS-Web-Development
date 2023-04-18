2058babcf911f0c45465dd87690431e9ffd116e8
batanoff <batanoff.s@protonmail.com>
2023-04-18 17:12:11 +0300

    edit

---
+++ 02. Fundamentals/08. Arrays - Exercise/Optional.LadyBug.js
@@ -0,0 +1,21 @@
+function ladybugs() {
+
+}
+
+ladybugs([ 3, '0 1',
+         '0 right 1',
+         '2 right 1' ]
+);
+console.log(`----------------`);
+
+ladybugs([ 3, '0 1 2',
+         '0 right 1',
+         '1 right 1',
+         '2 right 1']
+);
+console.log(`----------------`);
+
+ladybugs([ 5, '3',
+         '3 left 2',
+         '1 left -2']
+);
