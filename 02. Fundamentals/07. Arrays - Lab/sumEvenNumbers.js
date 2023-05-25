4f067bc02c829310abae54def5be7a74a3a20d43
batanoff <batanoff.s@protonmail.com>
2023-05-25 22:40:00 +0300

    sumEvenNumbers update

--- 02. Fundamentals/07. Arrays - Lab/sumEvenNumbers.js
+++ 02. Fundamentals/07. Arrays - Lab/sumEvenNumbers.js
@@ -1,11 +1,23 @@
-function sumEvenNumbers (arr) {
+// function sumEvenNumbers (arr) {
+//   let sum = 0;
+
+//   for(let i = 0; i < arr.length; i++) {    
+//     let currentNumber = parseInt(arr[i]);
+//     if (currentNumber % 2 === 0) {
+//       sum += currentNumber;
+//     }    
+//   }
+//   console.log(sum);
+// }
+// sumEvenNumbers(['1','2','3','4','5','6']);
+
+function sum (numAsText) {
   let sum = 0;
-  for(let i = 0; i < arr.length; i++) {    
-    let currentNumber = parseInt(arr[i]);
-    if (currentNumber % 2 === 0) {
-      sum += currentNumber;
-    }    
+  
+  for(let text of numAsText) {    
+    let num = Number(text);
+    sum += num % 2 === 0 ? num : 0;        
   }
   console.log(sum);
 }
-sumEvenNumbers(['1','2','3','4','5','6']);
+sum(['1','2','3','4','5','6']);
