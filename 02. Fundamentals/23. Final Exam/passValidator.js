e6ab3fdd1fe6cc1337461bf0e362c4eb020ae7f9
batanoff <batanoff.s@protonmail.com>
2023-08-14 16:06:33 +0300

    next advanced JS

--- 02. Fundamentals/23. Final Exam/passValidator.js
+++ 02. Fundamentals/23. Final Exam/passValidator.js
@@ -1,12 +1,11 @@
 function passValidator(input) {
-
    let userPassowrd = input.shift(); //invalidpassword*
 
    for (const entry of input) {
       let token = entry.split(" ");
       let command = token[0];
 
-      if (command === 'Make') {
+      if (command === "Make") {
          command += ` ${token[1]}`;
       }
 
@@ -15,7 +14,7 @@ function passValidator(input) {
       let letter = "";
       let lastPart = "";
 
-      if(command !== "Complete") {
+      if (command !== "Complete") {
          switch (command) {
             case `Make Upper`:
                index = Number(token[2]);
@@ -30,59 +29,63 @@ function passValidator(input) {
             case `Make Lower`:
                index = Number(token[2]);
                let a = index + 1;
-               
+
                fisrtPart = userPassowrd.substring(0, index);
                letter = userPassowrd.substring(a, 3).toLowerCase();
                lastPart = userPassowrd.substring(index + 1);
                userPassowrd = fisrtPart + letter + lastPart;
                console.log(userPassowrd);
-               
+
                break;
 
             case `Insert`:
                let charIndex = token[2];
                index = Number(token[1]);
 
-               if(index) {
-               fisrtPart = userPassowrd.substring(0, index);
-               lastPart = userPassowrd.substring(index);
-               userPassowrd = fisrtPart + charIndex + lastPart;
-               console.log(userPassowrd);
+               if (index) {
+                  fisrtPart = userPassowrd.substring(0, index);
+                  lastPart = userPassowrd.substring(index);
+                  userPassowrd = fisrtPart + charIndex + lastPart;
+                  console.log(userPassowrd);
                }
-               
+
                break;
 
             case `Replace`:
-               
                let charReplace = token[1];
-               if(userPassowrd.includes(charReplace)) {
-               let value = Number(token[2]);               
-               let asciCode = charReplace.charCodeAt(0);
-               let newValue = value + asciCode;
-               let newChar = String.fromCharCode(newValue);
+               if (userPassowrd.includes(charReplace)) {
+                  let value = Number(token[2]);
+                  let asciCode = charReplace.charCodeAt(0);
+                  let newValue = value + asciCode;
+                  let newChar = String.fromCharCode(newValue);
 
                   for (const el of userPassowrd) {
                      if (el === charReplace) {
-                        userPassowrd = userPassowrd.replace(charReplace, newChar);
+                        userPassowrd = userPassowrd.replace(
+                           charReplace,
+                           newChar
+                        );
                      }
-                  }                  
+                  }
                }
                console.log(userPassowrd);
                break;
 
             case `Validation`:
-               if(!(userPassowrd.length > 8)) {
+               if (!(userPassowrd.length > 8)) {
                   console.log("Password must be at least 8 characters long!");
                }
 
                let pattern = /(\w*\d*)/g;
                let containsValidDate = pattern.test(userPassowrd);
-               if(!containsValidDate) {
-                  console.log("Password must consist only of letters, digits and _!");
+               if (!containsValidDate) {
+                  console.log(
+                     "Password must consist only of letters, digits and _!"
+                  );
                }
-                              
+
                // "Password must consist at least one uppercase letter!"
-               // console.log("Password must consist at least one lowercase letter!"); 
+               // console.log("Password must consist at least one lowercase letter!");
                // "Password must consist at least one digit!"
                break;
          }
@@ -91,21 +94,22 @@ function passValidator(input) {
 }
 
 passValidator([
-   'invalidpassword*',
-   'Add 2 p',
-   'Replace i -50',
-   'Replace * 10',
-   'Make Upper 2',
-   'Validation',
-   'Complete']);
-
+   "invalidpassword*",
+   "Add 2 p",
+   "Replace i -50",
+   "Replace * 10",
+   "Make Upper 2",
+   "Validation",
+   "Complete",
+]);
 
 console.log(`----------------------`);
 
 passValidator([
-   '123456789',
-   'Insert 3 R',
-   'Replace 5 15',
-   'Validation',
-   'Make Lower 3',
-   'Complete']);
+   "123456789",
+   "Insert 3 R",
+   "Replace 5 15",
+   "Validation",
+   "Make Lower 3",
+   "Complete",
+]);
