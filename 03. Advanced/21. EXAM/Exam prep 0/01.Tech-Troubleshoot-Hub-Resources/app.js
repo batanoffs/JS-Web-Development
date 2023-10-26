ed4ea09f89cda5d6331a1dfa41fc45a34b109e8a
batanoffs <batanoff.s@protonmail.com>
2023-10-26 06:21:34 +0300

    practice dom

--- 03. Advanced/21. EXAM/Exam prep 0/01.Tech-Troubleshoot-Hub-Resources/app.js
+++ 03. Advanced/21. EXAM/Exam prep 0/01.Tech-Troubleshoot-Hub-Resources/app.js
@@ -1,71 +1,117 @@
 window.addEventListener("load", solution);
 
 function solution() {
-    //selectors
-
     const form = document.querySelector("form");
     const previewUL = document.querySelector(".preview-list");
+    const pendingUL = document.querySelector(".pending-list");
+    const resolvedList = document.querySelector(".resolved-list");
     const addBtn = form[5];
-    let counter = 0;
+    let employee = form[0];
+    let category = form[1];
+    let urgency = form[2];
+    let assignedTeam = form[3];
+    let description = form[4];
 
     addBtn.addEventListener(`click`, add);
 
     function add(ev) {
         ev.preventDefault();
 
-        let employee = form[0].value;
-        let category = form[1].value;
-        let urgency = form[2].value;
-        let assignedTeam = form[3].value;
-        let description = form[4].value;
-
         if (
-            !employee ||
-            !category ||
-            !urgency ||
-            !assignedTeam ||
-            !description
+            !employee.value ||
+            !category.value ||
+            !urgency.value ||
+            !assignedTeam.value ||
+            !description.value
         ) {
             return;
         }
-        let li = document.createElement("li");
-        let editBtn = document.createElement("button");
-        let continueBtn = document.createElement("button");
-        let myArticle = createArticle(
-            employee,
-            category,
-            urgency,
-            assignedTeam,
-            description
+
+        let listArticle = createArticle(
+            employee.value,
+            category.value,
+            urgency.value,
+            assignedTeam.value,
+            description.value
         );
 
-        li.classList = "preview-list";
-        editBtn.classList = "edit-btn";
+        function createArticle(e, c, u, team, d) {
+            let article = document.createElement("article");
+            article.innerHTML = `
+                <p>Form: ${e}</p>
+                <p>Category: ${c}</p>
+                <p>Urgency: ${u}</p>
+                <p>Assigned to: ${team}</p>
+                <p>Description: ${d}</p>
+                `;
+            return article;
+        }
+
+        let liContent = document.createElement("li");
+        liContent.setAttribute("class", "problem-content");
+
+        let editBtn = document.createElement(`button`);
+        editBtn.setAttribute("class", "edit-btn");
         editBtn.textContent = "Edit";
+
+        let continueBtn = document.createElement(`button`);
+        continueBtn.setAttribute("class", "continue-btn");
         continueBtn.textContent = "Continue";
-        continueBtn.classList = "continue-btn";
-        li.appendChild(myArticle);
-        li.appendChild(editBtn);
-        li.appendChild(continueBtn);
-        previewUL.appendChild(li);
+
+        liContent.appendChild(listArticle);
+        liContent.appendChild(editBtn);
+        liContent.appendChild(continueBtn);
+        previewUL.appendChild(liContent);
 
         form[0].value = ``;
         form[1].value = ``;
         form[2].value = ``;
         form[3].value = ``;
         form[4].value = ``;
-        counter++;
-        addBtn.textContent = counter;
-    }
-    function createArticle(e, c, u, team, d) {
-        let article = document.createElement("article");
-        article.innerHTML = `
-        <p>Form: ${e}</p>
-        <p>Category: ${c}</p>
-        <p>Urgency: ${u}</p>
-        <p>Assigned to: ${team}</p>
-        <p>Description: ${d}</p>
-        `;
-        return article;
+        addBtn.disabled = true;
+        
+        editBtn.addEventListener(`click`, onEdit);
+        continueBtn.addEventListener(`click`, pending);
+
+        function onEdit() {
+            let articleArray = Array.from(liContent.children[0].children);
+            let articleValues = [];
+            for (const el of articleArray) {
+                articleValues.push(el.textContent);
+            }
+            let employeePreview = articleValues[0].split("Form: ")[1];
+            let categoryPreview = articleValues[1].split("Category: ")[1];
+            let urgencyPreview = articleValues[2].split("Urgency: ")[1];
+            let assignedPreview = articleValues[3].split("Assigned to: ")[1];
+            let descriptionPreview = articleValues[4].split("Description: ")[1];
+
+            employee.value = employeePreview;
+            category.value = categoryPreview;
+            urgency.value = urgencyPreview;
+            assignedTeam.value = assignedPreview;
+            description.value = descriptionPreview;
+
+            liContent.remove();
+            addBtn.disabled = false;
+        }
+        function pending() {
+            pendingUL.appendChild(liContent);
+            editBtn.remove();
+            continueBtn.textContent = `Resolved`;
+            continueBtn.classList = `resolve-btn`;
+            addBtn.disabled = false;
+            continueBtn.removeEventListener(`click`, pending);
+            continueBtn.addEventListener(`click`, resolve);
+        }
+        function resolve() {
+            resolvedList.appendChild(liContent);
+            continueBtn.textContent = `Clear`;
+            continueBtn.classList = `clear-btn`;
+            continueBtn.removeEventListener(`click`, resolve);
+            continueBtn.addEventListener(`click`, clear);
+        }
+        function clear() {
+            liContent.remove();
+        }
     }
 }
--- 03. Advanced/21. EXAM/Exam prep 3/01.Fitness Journey-Resources/app.js
+++ 03. Advanced/21. EXAM/Exam prep 3/01.Fitness Journey-Resources/app.js
@@ -1,9 +1,30 @@
-window.addEventListener('load', solve);
+window.addEventListener("load", solve);
 
 function solve() {
-//TODO...
+    //selectors
+    const nameField = document.getElementById("name");
+    const emailField = document.getElementById("email");
+    const contactNumberField = document.getElementById("contact-number");
+    const typeField = document.getElementById("class-type");
+    const timeField = document.getElementById("class-time");
+    const nextBtn = document.getElementById("next-btn");
+    const previewUl = document.getElementsByClassName("class-info");
+    const confirmUl = document.getElementsByClassName("confirm-class");
+
+    nextBtn.addEventListener(`click`, onSubmit);
+
+    function onSubmit(e) {
+        e.preventDefault();
+        if (
+            !nameField.value ||
+            !emailField.valie ||
+            !contactNumberField.value ||
+            !timeField.value ||
+            !typeField.value
+        ) {
+            return;
+        }
+        let list = document.createElement("li");
+        list.setAttribute(`class`, `confirm-class`);
+    }
 }
-
-
-
-
